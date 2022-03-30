4cb28ac7b97beda13a6f5615684ad414e8315d60
adriana <apatriciarojas@ucundinamarca.edu.co>
2022-03-30 15:18:30 -0500

    se muestra  en la tabla y  en orden

--- src/app/components/list-recicladores/list-recicladores.component.ts
+++ src/app/components/list-recicladores/list-recicladores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { RecicladorService } from 'src/app/services/reciclador.service';
 
 @Component({
   selector: 'app-list-recicladores',
@@ -8,10 +9,34 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list-recicladores.component.css']
 })
 export class ListRecicladoresComponent implements OnInit {
-  constructor() {
+  
+  recicladores: any[]=[];
+
+  constructor(private _recicladorService: RecicladorService) {
   }
 
   ngOnInit(): void {
+    this.getRecicladores()
+  }
+  
+  getRecicladores(){
+    this._recicladorService.getRecicladores().subscribe(data => {
+      this.recicladores=[];
+      //data.sort();
+      //data.reverse();
+      data.forEach((element:any)=> {
+        console.log(element.payload.doc.id);// obtengo id
+        console.log(element.payload.doc.data());//obtengo datos
+
+        this.recicladores.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      
+      console.log(this.recicladores);//obtengo datos
+    });   
+
   }
 
 }
