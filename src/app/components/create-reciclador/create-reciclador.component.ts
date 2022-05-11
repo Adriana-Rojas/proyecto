db20a236bf7b0d1c102669f199a58b7e64532f8e
adriana <apatriciarojas@ucundinamarca.edu.co>
2022-05-10 23:19:46 -0500

    NO FUNCIONA

--- src/app/components/create-reciclador/create-reciclador.component.ts
+++ src/app/components/create-reciclador/create-reciclador.component.ts
@@ -9,53 +9,87 @@ import { RecicladorService } from 'src/app/services/reciclador.service';
   templateUrl: './create-reciclador.component.html',
   styleUrls: ['./create-reciclador.component.css']
 })
-export class CreateRecicladorComponent implements OnInit {
-  createReciclador: FormGroup;
-  submitted =false;
-  loading =false;
-  id: string|null;
-  titulo='Agregar Reciclador';
-  titulobtn='Agregar';
 
-  constructor( private fb :FormBuilder,
-              private _recicladorService: RecicladorService,
-              private router: Router,
-              private toastr: ToastrService,
-              private aRoute:ActivatedRoute) { 
-      this.createReciclador=this.fb.group({
-        nombre: ['',Validators.required],
-        apellido: ['',Validators.required],
-        documento: ['',Validators.required],
-        fecha: ['',Validators.required],
-        telefono: ['',Validators.required],
-        correo: ['',Validators.required],
-        contrasena: ['',Validators.required]
-      }
-      )
-      this.id=this.aRoute.snapshot.paramMap.get('id');
-      console.log(this.id);
+
+export class CreateRecicladorComponent implements OnInit {
+  registrationForm: FormGroup;
+  fieldTextType = false;
+  repeatFieldTextType = false;
+  createReciclador: FormGroup;
+  submitted = false;
+  loading = false;
+  id: string | null;
+  titulo = 'Agregar Reciclador';
+  titulobtn = 'Agregar';
+
+  hide: boolean = true;
+  visible = false;
+toggle(event){
+    this.visible = !this.visible;
+}
+
+  myFunction() {
+    this.hide = !this.hide;
   }
+
+  constructor(private fb: FormBuilder,
+    private _recicladorService: RecicladorService,
+    private router: Router,
+    private toastr: ToastrService,
+    private aRoute: ActivatedRoute) {
+    this.createReciclador = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      documento: ['', Validators.required],
+      fecha: ['', Validators.required],
+      telefono: ['', Validators.required],
+      correo: ['', Validators.required],
+      contrasena: ['', Validators.required]
+    }
+    )
+    this.registrationForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      confirmpassword: ["", Validators.required]
+    });
+    this.id = this.aRoute.snapshot.paramMap.get('id');
+    console.log(this.id);
+  }
+
 
   ngOnInit(): void {
     this.esEditar();
   }
 
-  agregarEditarReciclador(){
-    this.submitted=true;
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
 
-    if(this.createReciclador.invalid){
+    this.fieldTextType = true;
+  }
+
+  toggleRepeatFieldTextType() {
+    this.repeatFieldTextType = true;
+    this.repeatFieldTextType = !this.repeatFieldTextType;
+    
+   
+  }
+
+  agregarEditarReciclador() {
+    this.submitted = true;
+
+    if (this.createReciclador.invalid) {
       return;
     }
 
-    if (this.id==null) {
+    if (this.id == null) {
       this.agregarReciclador();
     } else {
       this.editarReciclador(this.id);
     }
   }
 
-  agregarReciclador(){
-    const reciclador:any={
+  agregarReciclador() {
+    const reciclador: any = {
       nombre: this.createReciclador.value.nombre,
       apellido: this.createReciclador.value.apellido,
       documento: this.createReciclador.value.documento,
@@ -63,27 +97,27 @@ export class CreateRecicladorComponent implements OnInit {
       telefono: this.createReciclador.value.telefono,
       correo: this.createReciclador.value.correo,
       contrasena: this.createReciclador.value.contrasena,
-      fechaCreacion:new Date(),
-      fechaActualizacion:new Date()
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
     }
-    
-    this.loading=true;
 
-    this._recicladorService.agregarReciclador(reciclador).then(()=>{
-      this.toastr.success('Reciclador Registrado', 'Creado!',{positionClass: 'toast-bottom-right'});
-      this.loading=false;
+    this.loading = true;
+
+    this._recicladorService.agregarReciclador(reciclador).then(() => {
+      this.toastr.success('Reciclador Registrado', 'Creado!', { positionClass: 'toast-bottom-right' });
+      this.loading = false;
       this.router.navigate(['/list-recicladores'])
-    }).catch(error =>{
+    }).catch(error => {
       console.log(error);
-      this.loading=false;
-      this.toastr.error('Reciclador Error Registro', 'Error!',{positionClass: 'toast-bottom-right'});
+      this.loading = false;
+      this.toastr.error('Reciclador Error Registro', 'Error!', { positionClass: 'toast-bottom-right' });
     });
   }
 
 
-  editarReciclador(id:string){
+  editarReciclador(id: string) {
 
-    const reciclador:any={
+    const reciclador: any = {
       nombre: this.createReciclador.value.nombre,
       apellido: this.createReciclador.value.apellido,
       documento: this.createReciclador.value.documento,
@@ -91,25 +125,25 @@ export class CreateRecicladorComponent implements OnInit {
       telefono: this.createReciclador.value.telefono,
       correo: this.createReciclador.value.correo,
       contrasena: this.createReciclador.value.contrasena,
-      fechaActualizacion:new Date()
+      fechaActualizacion: new Date()
     }
 
-    this.loading=false;
-    this._recicladorService.actualizarReciclador(id,reciclador).then(()=>{
-      this.loading=false;
-      this.toastr.info('Reciclador Editado', 'Editado!',{positionClass: 'toast-bottom-right'});
+    this.loading = false;
+    this._recicladorService.actualizarReciclador(id, reciclador).then(() => {
+      this.loading = false;
+      this.toastr.info('Reciclador Editado', 'Editado!', { positionClass: 'toast-bottom-right' });
     });
     this.router.navigate(['/list-recicladores'])
 
   }
 
-  esEditar(){
-    this.titulo='Editar Reciclador';
-    this.titulobtn='Actualizar';
-    if(this.id != null){
-      this.loading=true;
-      this._recicladorService.getReciclador(this.id).subscribe(data =>{
-        this.loading=false;
+  esEditar() {
+    this.titulo = 'Editar Reciclador';
+    this.titulobtn = 'Actualizar';
+    if (this.id != null) {
+      this.loading = true;
+      this._recicladorService.getReciclador(this.id).subscribe(data => {
+        this.loading = false;
         console.log(data.payload.data()['nombre']);
         this.createReciclador.setValue({
           nombre: data.payload.data()['nombre'],
@@ -123,5 +157,7 @@ export class CreateRecicladorComponent implements OnInit {
       })
     }
   }
- 
+
+
+
 }
